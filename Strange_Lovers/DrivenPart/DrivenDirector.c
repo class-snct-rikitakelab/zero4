afb8090b3c7626cf20f97645d1df72eff9e959d1
hiromitsu <hitsk26@gmail.com>
2013-10-09 15:12:08 +0900

    ロボコン用のモデルシートなどのフォルダを削除

--- Strange_Lovers/DrivenPart/DrivenDirector.c
+++ Strange_Lovers/DrivenPart/DrivenDirector.c
@@ -18,16 +18,7 @@ float DrivenDirector_calc_turn_value(DrivenDirector *self,float target_brightnes
 	LV_setTargLightVal(&mLightVal,target_brightness);
 	PLVC_setMovementDirection(mLightValCtrl.mPIDLightValCtrl,movementDirection);
 	PCC_setMovementDirection(mCurvatureCtrl.mPIDCurvatureCtrl,movementDirection);
-	float brightness_turn = LVC_run(&mLightValCtrl);
-	float curvature_turn = CC_run(&mCurvatureCtrl);
 	float direction_turn = DirectionCtrl_run(&directionCtrl,target_runner_angle);
-	float turn_sum =use_controller.target_light_controller_weight*brightness_turn  
-		+  use_controller.target_curvature_controller_weight*curvature_turn + use_controller.target_runner_angle_controller_weight*direction_turn;
-	
-	//logSend(0,DirectionEncoder_get_direction(&directionEncoder),CurvatureEncoder_get_curvature(&curvatureEncoder)*1000,DistanceEncoder_get_total_distance(&distanceEncoder),CoordinateEncoder_get_xCoo(&coordinateEncoder),CoordinateEncoder_get_yCoo(&coordinateEncoder),0,0);
-	//logSend(0,0, turn_sum,0,SpeedEncoder_get_speed(&speedEncoder),DirectionEncoder_get_direction(&directionEncoder),0,0,0);
-	ecrobot_debug1(direction_turn,turn_sum,0);
-
 
 	return direction_turn;
 }
